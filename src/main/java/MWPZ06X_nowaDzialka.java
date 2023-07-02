338ca94930ed39f416cf9a8328d3d156235aca4b
pleewson <adrianplewait@gmail.com>
2023-07-02 17:50:56 +0200

    nowaDzialka -accepted

---
+++ src/main/java/MWPZ06X_nowaDzialka.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MWPZ06X_nowaDzialka {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int D = scan.nextInt();
+
+        for(int i = 0; i < D; i++){
+            int x = scan.nextInt();
+            int area = x * x;
+
+            System.out.println(area);
+        }
+
+
+    }
+}
