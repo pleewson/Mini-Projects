1a98bdbdb47fcbc52b81f86e4fd30728f213539d
pleewson <adrianplewait@gmail.com>
2023-07-18 19:23:03 +0200

    new task started

---
+++ src/main/java/PP0604A_sredniaArytmetyczna.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PP0604A_sredniaArytmetyczna {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+
+        if(t >= 0 && t <= 100){ // testy
+            int n = scan.nextInt(); //data
+
+            if(n > 0 && n < 100){
+                
+            }
+
+        }
+
+    }
+}
