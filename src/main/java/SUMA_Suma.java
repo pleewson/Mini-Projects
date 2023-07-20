df2c3894e10f5333a56f271892d163fdcc2b1300
pleewson <adrianplewait@gmail.com>
2023-07-20 20:49:05 +0200

    SUMA - spoj task

---
+++ src/main/java/SUMA_Suma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SUMA_Suma {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int t = scan.nextInt();
+
+        int sum = 0;
+
+        if(t >= 0 && t <= 110){
+            for(int i = 0; i < t; i++){
+                int value = scan.nextInt();
+                sum += value;
+                System.out.println(sum);
+            }
+        }
+    }
+}
+//not accepted
+//https://pl.spoj.com/problems/SUMA/
