511e92e941b80be64643b23fccfeec9cf5fc6dab
pleewson <adrianplewait@gmail.com>
2023-07-26 18:47:20 +0200

    zadanieProbne accepted

---
+++ src/main/java/PTEST_zadanieProbne.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PTEST_zadanieProbne {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        if(A < 200 && B < 200){
+            System.out.println(A + B);
+        }
+
+    }
+}
